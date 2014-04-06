147dedb468472eb80ab598d71195dd167ac7ed3a
Jonathan Chang <jonoschang7@gmail.com>
2014-04-06 00:37:08 -0400

    Show contact route and view done

--- crm.rb
+++ crm.rb
@@ -4,6 +4,8 @@ require 'sinatra'
 
 @@rolodex = Rolodex.new
 
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+
 get "/" do
 	@crm_app_name = "Wonderful World of CRM"
 	erb :index
@@ -21,6 +23,11 @@ post '/contacts' do
 	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   @@rolodex.add_contact(new_contact)
   redirect to('/contacts')
+end
+
+get "/contacts/1000" do
+	@contact = @@rolodex.find(1000)
+	erb :show_contact
 end
 
 get "/contacts/:id" do
