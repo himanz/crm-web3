8663100bfcd117a618368779d4b9beb23d17c3e0
Jonathan Chang <jonoschang7@gmail.com>
2014-04-05 23:20:54 -0400

    Change how to get date data

--- contact.rb
+++ contact.rb
@@ -1,10 +1,10 @@
 class Contact
   attr_accessor :first_name, :last_name, :email, :note, :id, :date
-	def initialize(first_name, last_name, email, note, date)
+	def initialize(first_name, last_name, email, note)
 		@first_name = first_name
 		@last_name = last_name
 		@email = email
 		@note = note
-		@date = date
+		@date = Time.now
 	end 
 end
