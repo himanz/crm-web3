b937bee1ee1cb6e6b93cb96eff59521fb883387b
Jonathan Chang <jonoschang7@gmail.com>
2014-04-05 23:09:03 -0400

    Add contacts route and view

--- crm.rb
+++ crm.rb
@@ -1,8 +1,18 @@
+require_relative 'contact'
 require 'sinatra'
 
 get "/" do
 	@crm_app_name = "Wonderful World of CRM"
 	erb :index
+end
+
+get "/contacts" do
+	@contacts = []
+	@contacts << Contact.new("Julie", "Hache" , "julie@bitmakerlabs.com", "Instructor")
+	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+
+  erb :contacts
 end
 
 get "/contacts/new" do
